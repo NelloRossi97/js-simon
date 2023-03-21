759637d7dae4dc909d95d4aeecd57e68de2596eb
NelloRossi97 <rossianiello97@gmail.com>
2023-03-21 15:34:16 +0100

    added function start game/hide numbers/show input

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, i numeri che ha visto precedentemente.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+`<div class="col">
+<div class="card d-flex justify-content-center align-items-center fs-1">ciao</div>
+</div>`
+
 */
+
+const numsBox = document.querySelector('.row');
+const numsToGenerate = 5;
+const start = document.querySelector('.btn-primary');
+start.addEventListener('click', startGame);
+const resultBox = document.querySelector('.result');
+const controlBtn = document.querySelector('.btn-warning');
+controlBtn.addEventListener('click', checkResult);
---
+++ js/utility.js
@@ -0,0 +1,36 @@
+//funzione per generare un numero random
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+//funzione play
+function startGame(){
+    numsBox.innerHTML = "";
+    const numbers = [];
+    for (let i = 0; i < numsToGenerate ; i++){
+        const num = getRndInteger(1,10);
+        numsBox.innerHTML += `<div class="col">
+        <div class="card d-flex justify-content-center align-items-center fs-1">${num}</div>
+        </div>`;
+        numbers.push(num);
+    }
+    setTimeout(hideNumbers, 2000);
+}
+
+//funzione per cancellare i numeri e far apparire gli input
+function hideNumbers(){
+    numsBox.innerHTML = "";
+    resultBox.classList.remove('opacity-0');
+}
+
+function checkResult(){
+    resultBox.classList.add('opacity-0');
+    const textBox = document.querySelector('input[type=text]');
+    let resultValue = textBox.value;
+    let resultArray = resultValue.split(" ");
+    for (let i = 0; i < resultArray.length; i++){
+        if (resultArray[i].includes(numbers[i])){
+            console.log('ciao');
+        }
+    }
+}
