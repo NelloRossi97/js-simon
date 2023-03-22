3c7dbcc88e60ba63fcd91b2ca68b50c28065b138
NelloRossi97 <rossianiello97@gmail.com>
2023-03-22 21:07:47 +0100

    fixed comparison of rnd numbers and user numbers

--- js/utility.js
+++ js/utility.js
@@ -11,7 +11,7 @@ function startGame(){
     for (let i = 0; i < numsToGenerate ; i++){
         const num = getRndInteger(1,10);
         numsBox.innerHTML += `<div class="col">
-        <div class="card d-flex justify-content-center align-items-center fs-1">${num}</div>
+        <div class="card d-flex justify-content-center align-items-center fs-1 text-black">${num}</div>
         </div>`;
         numbers.push(num);
     }
@@ -24,19 +24,25 @@ function hideNumbers(){
     resultBox.classList.remove('opacity-0');
 }
 
+
 function checkResult(){
     resultBox.classList.add('opacity-0');
+    let guessedNumbers = [];
     const textBox = document.querySelector('input[type=text]');
     let resultValue = textBox.value;
     let resultArray = resultValue.split(" ");
     for (let i = 0; i < resultArray.length; i++){
-        if (resultArray[i].includes(numbers[i])){
-            endGameBox.innerHTML = `<h2 class="text-success text-center">Complimenti, hai vinto! Hai una buona memoria!</h2>`;
+        if (numbers.includes(parseInt(resultArray[i]))){
+            guessedNumbers.push(resultArray[i]);
         }
-        else{
-            endGameBox.innerHTML = `<h2 class="text-danger text-center">Mi dispiace, ma i numeri che hai inserito non sono gli stessi</h2>
-            <h3 class="text-center"> I numeri generati erano: ${numbers} e tu hai inserito: ${resultArray}`;
-        }
+    }
+    console.log(guessedNumbers, 'ciao');
+    if(guessedNumbers.length === numbers.length){
+        endGameBox.innerHTML = `<h2 class="text-success text-center">Complimenti, hai vinto! Hai indovinato tutti i numeri!</h2>`
+    } else{
+        endGameBox.innerHTML = `
+        <h2 class="text-danger text-center">Mi dispiace, ma i numeri che hai inserito non sono gli stessi</h2>
+        <h3 class="text-center"> I numeri generati erano: ${numbers} e tu hai inserito: ${resultArray}. Il tuo punteggio, quindi è: ${guessedNumbers.length}`;
     }
     numbers = [];
 }
